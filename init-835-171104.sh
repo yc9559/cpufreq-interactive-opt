af408cfcf1bc1dbd43afcdf4f4c9e8081e17ee76
Matt Yang <yc9559@hotmail.com>
2017-11-06 22:33:08 +0800

    import cmd from xiaomi mi6 init.rc

--- init-835-171104.sh
+++ init-835-171104.sh
@@ -63,6 +63,31 @@ case "$target" in
         echo 0 > /sys/module/msm_thermal/core_control/enabled
         chmod 0444 /sys/module/msm_thermal/core_control/enabled
 
+    # from init.rc in xiaomi mi 6
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/timer_rate
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/timer_rate
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/timer_slack
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/timer_slack
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/min_sample_time
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/min_sample_time
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/hispeed_freq
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/hispeed_freq
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/target_loads
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/target_loads
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/go_hispeed_load
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/go_hispeed_load
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/above_hispeed_delay
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/above_hispeed_delay
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/boost
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/boost
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/boostpulse
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/input_boost
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/input_boost
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/boostpulse_duration
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/boostpulse_duration
+    chown root root /sys/devices/root/cpu/cpufreq/interactive/io_is_busy
+    chmod 0660 /sys/devices/root/cpu/cpufreq/interactive/io_is_busy
+
         # online CPU0
         echo 1 > /sys/devices/system/cpu/cpu0/online
 	# configure governor settings for little cluster
@@ -163,19 +188,31 @@ case "$target" in
         echo "0:0 1:0 2:0 3:0 4:2208000 5:0 6:0 7:0" > /sys/module/cpu_boost/parameters/powerkey_input_boost_freq
         echo 400 > /sys/module/cpu_boost/parameters/powerkey_input_boost_ms
 
-	echo N > /sys/module/lpm_levels/system/pwr/cpu0/ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/pwr/cpu1/ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/pwr/cpu2/ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/pwr/cpu3/ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/perf/cpu4/ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/perf/cpu5/ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/perf/cpu6/ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/perf/cpu7/ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-dynret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/pwr/pwr-l2-ret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/perf/perf-l2-dynret/idle_enabled
-	echo N > /sys/module/lpm_levels/system/perf/perf-l2-ret/idle_enabled
-	echo N > /sys/module/lpm_levels/parameters/sleep_disabled
+    # from init.rc in xiaomi mi 6
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/timer_rate
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/timer_rate
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/timer_slack
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/timer_slack
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/target_loads
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/target_loads
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/boost
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/boost
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/boostpulse
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/input_boost
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/input_boost
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/boostpulse_duration
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/boostpulse_duration
+    chown system system /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+    chmod 0660 /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+
         echo 0 > /dev/cpuset/background/cpus
         echo 0-2 > /dev/cpuset/system-background/cpus
         echo 4-7 > /dev/cpuset/foreground/boost/cpus
